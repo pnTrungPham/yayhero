f04100498ae002a15fffb7730da2beb6144d293f
Shelby <lvtute@gmail.com>
2023-03-07 16:53:51 +0700

    first commit

---
+++ yayhero.php
@@ -0,0 +1,32 @@
+
+<?php
+
+/**
+ * Plugin Name:     Train Yay Hero
+ * Plugin URI:      https://yaycommerce.com/
+ * Description:     Starter plugin. Training basic WordPress plugin development.
+ * Author:          Yay Commerce
+ * Author URI:      https://yaycommerce.com/
+ * Text Domain:     yayhero
+ * Domain Path:     /languages
+ * Version:         1.0.0.3
+ *
+ * @package Yayhero
+ */
+
+if (!defined('ABSPATH')) {
+    die('We\'re sorry, but you can not directly access this file.');
+}
+
+define('YAY_HERO_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('YAY_HERO_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+if (!wp_installing()) {
+
+    add_action(
+        'plugins_loaded',
+        function () {
+            include YAY_HERO_PLUGIN_PATH . 'includes/pages/hero-admin.php';
+        }
+    );
+}
