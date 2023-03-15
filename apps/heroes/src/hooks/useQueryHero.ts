43ab096d9a70a1191e606dcdd3b698a060d2a397
Shelby <lvtute@gmail.com>
2023-03-15 16:18:15 +0700

    remove unused code

--- apps/heroes/src/hooks/useQueryHero.ts
+++ apps/heroes/src/hooks/useQueryHero.ts
@@ -1,8 +1,6 @@
 import { getHeroById } from "@src/api/heroEndpoint";
-import { useState } from "react";
 import { useQuery } from "react-query";
 import { STALE_TIME } from "./constants";
-import { HeroModel } from "@src/types/heroes.type";
 
 export default function useQueryHero(id: number) {
   return useQuery(["hero", id], () => getHeroById(id), {
