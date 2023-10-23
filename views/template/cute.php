e3b0bc1dcb8a9d3c149f5142c018481c6dc1e5f7
pnTrungPham <65383916+pnTrungPham@users.noreply.github.com>
2023-10-23 08:33:14 +0700

    Update code

--- views/template/cute.php
+++ views/template/cute.php
@@ -5,25 +5,32 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 
+<div class="text-slide">
+	Hãy nhấn và di chuyển chuột :))))
+</div>
+
 <div id="drag-container">
-        <div id="spin-container">
-			<img src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh.jpeg')?>" alt="">
-			<img src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh.jpeg')?>" alt="">
-			<img src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh.jpeg')?>" alt="">
-			<img src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh.jpeg')?>" alt="">
-			<img src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh.jpeg')?>" alt="">
-			<img src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh.jpeg')?>" alt="">
-			<img src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh.jpeg')?>" alt="">
-			<img src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh.jpeg')?>" alt="">
-			<p>Thanh Thanh</p>
-        </div>
-        <div id="ground"></div>
-      </div>
-      
-      <div id="music-container"></div>
-      <div id="canva">
-      <canvas id="canvas"></canvas>
-    </div>
-    <audio id="autoplay" controls autoplay>
-        <source src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/frontend/music.mp3')?>" >
-    </audio>
+	<div id="spin-container">
+		<img class="img-slide" src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh.jpeg')?>" alt="">
+		<img class="img-slide" src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh2.jpg')?>" alt="">
+		<img class="img-slide" src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh3.jpg')?>" alt="">
+		<img class="img-slide" src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh4.jpg')?>" alt="">
+		<img class="img-slide" src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh5.jpg')?>" alt="">
+		<img class="img-slide" src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh6.jpg')?>" alt="">
+		<img class="img-slide" src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh7.jpg')?>" alt="">
+		<img class="img-slide" src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh9.jpg')?>" alt="">
+		<img class="img-slide" src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/image/thanh_thanh10.jpg')?>" alt="">
+		<p>Thanh Thanh</p>
+	</div>
+	<div id="ground"></div>
+	</div>
+	
+	<div id="music-container"></div>
+	<div id="canva">
+	<canvas id="canvas">
+	
+	</canvas>
+</div>
+<audio id="autoplay" controls autoplay>
+	<source src="<?php echo  ( YAYMAIL_ADDON_PLUGIN_NAME_PLUGIN_URL . 'assets/frontend/music.mp3')?>" >
+</audio>
--- views/template/home.php
+++ views/template/home.php
@@ -6,6 +6,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 
 <div class="content">
+	<div class="text-slide">
+		Chữ chạy từ trái qua phải! Chữ chạy từ trái qua phải! Chữ chạy từ trái qua phải! Chữ chạy từ trái qua phải!
+	</div>
 	<img  src="https://img.freepik.com/free-vector/cute-cat-with-love-sign-hand-cartoon-illustration-animal-nature-concept-isolated-flat-cartoon-style_138676-3419.jpg?w=360" id="puppy">
 
 	<div class="wrap" style="margin-top: 30px">
