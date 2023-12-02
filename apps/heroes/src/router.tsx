63b64493465e215ea917242715b7f491cfaa85da
pnTrungPham <65383916+pnTrungPham@users.noreply.github.com>
2023-12-02 16:41:50 +0700

    Clean source

--- apps/heroes/src/router.tsx
+++ apps/heroes/src/router.tsx
@@ -1,16 +1,12 @@
 import { createHashRouter } from "react-router-dom";
 import HeroList from "./pages/HeroList";
-import BigManList from "./pages/BigManList";
 
 const router = createHashRouter([
-    {
-      path: "/heroes",
-      element: <HeroList />,
-    },
-    {
-      path: "/",
-      element: <BigManList />,
-    },
-  ]);
-  
-  export default router;
+  {
+    path: "/",
+    element: <HeroList />,
+  },
+  {},
+]);
+
+export default router;
