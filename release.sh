bed1233433520669e9a6d8860272b1ac9b347092
pnTrungPham <trung.nhatrangart@gmail.com>
2024-06-14 22:15:12 +0700

    Refactor release script to improve build process

--- release.sh
+++ release.sh
@@ -1,15 +1,16 @@
+#!/bin/bash
+
 echo "Generating build directory..."
 rm -rf "$(pwd)/release"
 mkdir -p "$(pwd)/release"
 
 echo "Syncing files..."
-cd ..
 rsync -rc --exclude-from="$(pwd)/.distignore" "$(pwd)/" "$(pwd)/release/wpinternallink" --delete --delete-excluded
 
 echo "Generating zip file..."
-cd release
+cd "$(pwd)/release"
 zip -q -r "wpinternallink.zip" "wpinternallink/"
 rm -rf wpinternallink
 echo "Generated release file"
 
-echo "Build successfully"
+echo "Build successful"
