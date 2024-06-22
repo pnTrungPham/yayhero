c7ee961776f55246d45bb053acd9611a15088974
pnTrungPham <trung.nhatrangart@gmail.com>
2024-06-22 20:26:05 +0700

    Refactor dashboard page to include logo and improve styling

--- templates/dashboard/dashboard.php
+++ templates/dashboard/dashboard.php
@@ -2,7 +2,11 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 
-<div class="wrap">
-    <h1><?php esc_html_e( 'Dashboard', 'wpinternallinks' ); ?></h1>
+<div class="wrap wpil-dashboard-page">
+    <div class="logo">
+        <img src="<?php echo esc_url( WP_INTERNAL_LINKS_PLUGIN_URL . 'assets/admin/images/logo.png' ); ?>" alt="WP Internal Links">
+        <h2>Report & Cluster Cockpit</h2>
+    </div>
+
     <div> <?php echo do_shortcode( '[wp_internal_links_post_list]' ); ?></div>
 </div>
